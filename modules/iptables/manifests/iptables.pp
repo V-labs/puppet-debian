f222633828eb691363592c569cac17106c395573
Valentin Ferriere <valentin.ferriere@gmail.com>
2013-07-31 00:13:21 +0200

    iptables again

--- modules/iptables/manifests/iptables.pp
+++ modules/iptables/manifests/iptables.pp
@@ -1,17 +1,35 @@
 class iptables::iptables {
-  file { "firewall":
-    ensure => present,
-    path    => "/etc/init.d/firewall",
-    owner   => 'root',
-    group   => 'root',
-    mode    => '755',
-    content => template('iptables/default.erb')
-  }
+    package {
+        "iptables-persistent": ensure => present;
+      }
 
-  service { "iptables":
-        enable => true,
-        require => [
-            File['/etc/init.d/firewall']
-        ]
-    }
+      service { "iptables-persistent":
+        require => Package["iptables-persistent"],
+
+        # Because there is no running process for this service, the normal status
+        # checks fail.  Because puppet then thinks the service has been manually
+        # stopped, it won't restart it.  This fake status command will trick puppet
+        # into thinking the service is *always* running (which in a way it is, as
+        # iptables is part of the kernel.)
+        hasstatus => true,
+        status => "true",
+
+        # Under Debian, the "restart" parameter does not reload the rules, so tell
+        # Puppet to fall back to stop/start, which does work.
+        hasrestart => false,
+
+      }
+
+      file {
+        "/etc/iptables/rules":
+          owner   => "root",
+          group   => "root",
+          mode    => 640,
+          content => template('iptables/default.erb'),
+          require => Package["iptables-persistent"],
+
+          # When this file is updated, make sure the rules get reloaded.
+          notify  => Service["iptables-persistent"],
+        ;
+      }
 }
